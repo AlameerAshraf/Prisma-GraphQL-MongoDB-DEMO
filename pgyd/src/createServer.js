0814368a50fcd5d700e13599836d202db67c8942
AlameerAshraf <alamiir.ashraf@gmail.com>
2018-11-22 21:19:56 +0200

    Create server GraphQL-Yoga

---
+++ pgyd/src/createServer.js
@@ -0,0 +1,20 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const Mutation = require('./Mutation');
+const Query = require('./Query');
+const db = require('./db');
+
+function CreateGraphQLServer(){
+
+    return new GraphQLServer({
+        typeDefs : '../src/schema.graphql',
+        resolvers : {
+            Mutation , Query
+        },
+        resolverValidationOptions : false,
+        context : req => ({req , db})
+    });
+}
+
+
+module.exports = CreateGraphQLServer;
